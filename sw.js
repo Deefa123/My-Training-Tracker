bc7e85aee2c3575be98d689029f3848d33de4d9d
Deefa123 <jwoodward0003@gmail.com>
2026-08-10 19:21:40 +0100

    Fix missing newline at end of sw.js
    Added a newline at the end of the file.

--- sw.js
+++ sw.js
@@ -2,3 +2,4 @@ const C='mtt-v15-ghpages';const A=['./','./index.html','./manifest.webmanifest',
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{let y=x.clone();caches.open(C).then(c=>c.put(e.request,y));return x}).catch(()=>caches.match('./index.html')))));
+ 
