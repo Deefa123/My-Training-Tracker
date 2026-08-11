072bfbdf8cf23f5233aaf21ec22e1681d19a8c14
Deefa123 <jwoodward0003@gmail.com>
2026-08-11 13:08:18 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,43 @@
-const C='mtt-v19-1-ghpages';const A=['./','./index.html','./manifest.webmanifest','./sw.js'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{let y=x.clone();caches.open(C).then(c=>c.put(e.request,y));return x}).catch(()=>caches.match('./index.html')))));
+const C='mtt-v19-2-ghpages';
+const A=['./','./index.html','./manifest.webmanifest','./sw.js'];
+
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',e=>{
+  const req=e.request;
+  const isNav=req.mode==='navigate' || (req.headers.get('accept')||'').includes('text/html');
+
+  if(isNav){
+    e.respondWith(
+      fetch(req,{cache:'no-store'})
+        .then(res=>{
+          const copy=res.clone();
+          caches.open(C).then(c=>c.put('./index.html',copy));
+          return res;
+        })
+        .catch(()=>caches.match('./index.html'))
+    );
+    return;
+  }
+
+  e.respondWith(
+    caches.match(req).then(cached=>{
+      if(cached)return cached;
+      return fetch(req).then(res=>{
+        const copy=res.clone();
+        caches.open(C).then(c=>c.put(req,copy));
+        return res;
+      });
+    })
+  );
+});
